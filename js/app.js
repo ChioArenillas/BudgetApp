4a51944c2ca535786a67f80af76e59945c53f604
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-02-16 11:30:25 +0100

    Initial commit BudgetApp

---
+++ js/app.js
@@ -0,0 +1,101 @@
+const incomes = [
+    new Income("Salary", 2100),
+    new Income("Classes", 250),
+]
+
+const expenses = [
+    new Expense("Home Rent", 450),
+    new Expense("Supermarcket", 200)
+]
+
+let loadApp = () => {
+    loadHeader()
+    loadIncomes()
+    loadExpenses()
+}
+let totalIncomes = () => {
+    let totalIncome = 0
+    for (let income of incomes) {
+        totalIncome += income.value
+    }
+    return totalIncome
+}
+let totalExpenses = () => {
+    let totalExpense = 0
+    for (let expense of expenses) {
+        totalExpense += expense.value
+    }
+    return totalExpense
+}
+let loadHeader = () => {
+    let budget = totalIncomes() - totalExpenses()
+    let expensePercentage = totalExpenses() / totalIncomes()
+    document.getElementById("budget").innerHTML = currencyFormat(budget)
+    document.getElementById("percentage").innerHTML = percentageFormat(expensePercentage)
+    document.getElementById("incomes").innerHTML = currencyFormat(totalIncomes())
+    document.getElementById("expenses").innerHTML = currencyFormat(totalExpenses())
+}
+
+const currencyFormat = (value) => {
+    return value.toLocaleString("en-US", { style: "currency", currency: "EUR", minimumFractionDigits: 2 })
+}
+const percentageFormat = (value) => {
+    return value.toLocaleString("en-US", { style: "percent", minimumFractionDigits: 2 })
+}
+
+const loadIncomes = () => {
+    let incomesHTML = ''
+    for (let income of incomes) {
+        incomesHTML += createIncomesHTML(income)
+    }
+    document.getElementById("incomes-list").innerHTML = incomesHTML
+}
+
+const createIncomesHTML = (income) => {
+    let incomeHTML = `
+        <div class="element clearStyles">
+            <div class="element_description">${income.description}</div>
+                <div class="right clearStyles">
+                    <div class="element_value">${currencyFormat(income.value)}</div>
+                        <div class="element_delete">
+                            <button class="element_delete--btn">
+                                <ion-icon name="close-outline"></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+
+    `
+    return incomeHTML
+}
+
+const loadExpenses = () => {
+    let expensesHTML = ''
+    for (let expense of expenses) {
+        expensesHTML += createExpenseHTML(expense)
+    }
+    document.getElementById("expenses-list").innerHTML = expensesHTML
+}
+
+const createExpenseHTML = (expense) => {
+    let expenseHTML = `
+        <div class="element clearStyles">
+            <div class="element_description">${expense.description}</div>
+                <div class="right clearStyles">
+                    <div class="element_value">${currencyFormat(expense.value)}</div>
+                    <div class="element_percentage">${percentageFormat(expense.value/totalExpenses())}</div>
+                        <div class="element_delete">
+                            <button class="element_delete--btn">
+                                <ion-icon name="close-outline"></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    `
+    return expenseHTML
+}
+
