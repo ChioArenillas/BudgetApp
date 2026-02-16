88de6af2e1b3bb0704fd7ba21dccf0cce4dd7759
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-02-16 11:51:34 +0100

    new

--- js/Data.js
+++ js/Data.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Data {
     constructor(description, value) {
         this._description = description
@@ -16,23 +15,4 @@ class Data {
     set value(velue){
         this._value = value
     }
-=======
-class Data {
-    constructor(description, value) {
-        this._description = description
-        this._value = value
-    }
-    get description(){
-        return this._description
-    }
-    set description(description){
-        this._description = description
-    }
-    get value(){
-        return this._value
-    }
-    set value(velue){
-        this._value = value
-    }
->>>>>>> d32d92745b4f2860f82359058b97db3892b5c065
 }
