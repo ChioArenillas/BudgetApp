208a077ba46a2757fe1df76fea79d9684ccd5d77
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-02-16 11:50:56 +0100

    new

--- js/Expense.js
+++ js/Expense.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Expense extends Data{
     static counterExpenses = 0
 
@@ -9,16 +8,4 @@ class Expense extends Data{
     get id(){
         return this._id
     }
-=======
-class Expense extends Data{
-    static counterExpenses = 0
-
-    constructor(description, value){
-        super(description, value)
-        this._id = ++Expense.counterExpenses
-    }
-    get id(){
-        return this._id
-    }
->>>>>>> d32d92745b4f2860f82359058b97db3892b5c065
 }
--- js/Income.js
+++ js/Income.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Income extends Data{
     static counterIncomes = 0
 
@@ -9,16 +8,4 @@ class Income extends Data{
     get id(){
         return this._id
     }
-=======
-class Income extends Data{
-    static counterIncomes = 0
-
-    constructor(description, value){
-        super(description, value)
-        this._id = ++Income.counterIncomes
-    }
-    get id(){
-        return this._id
-    }
->>>>>>> d32d92745b4f2860f82359058b97db3892b5c065
 }
